6880e3a55999251ba6ba6e648661c9fe19dd3df0
Sahasha Reese <codeHer@icloud.com>
2016-08-08 16:47:00 -0500

    first commit

---
+++ test/app_test.js
@@ -0,0 +1,21 @@
+var addOne = require(‘../src/app.js’).addOne;
+var expect = require(‘chai’).expect;
+
+
+describe(‘testing the addOne function’, function(){
+//“it” function calls an individual function
+//it(‘should add 1 to your argument’, fucntion(){
+//here is where our test code should go
+});
+var number = 42;
+var result = addOne(number);
+
+expect(result).to.equal(43);
+
+
+
+
+
+//it(…);
+
+// describer(…)
